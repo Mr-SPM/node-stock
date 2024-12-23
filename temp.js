821e818e3041481ef5c61fb19a05bf61eb33cfa8
weijie.com <vic.c2@outlook.com>
2024-12-23 21:11:24 +0800

    Update temp.js

--- temp.js
+++ temp.js
@@ -2,7 +2,7 @@ const { default: axios } = require('axios')
 const { Stocks, TempStocks, BaseSchema, LastStocks } = require('./model')
 const dayjs = require('dayjs')
 const a = require('iconv-lite');
-
+const data = require('./data')
 const batchSize = 1000;
 const utils = require('./utils')
 
